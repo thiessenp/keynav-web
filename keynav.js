80b98833d7a6117869906d9200dd6c24996a88d2
Peter Thiessen <60849273+thiessenp@users.noreply.github.com>
2020-09-28 12:13:09 -0400

    Adds some documentation to keynav

--- keynav.js
+++ keynav.js
@@ -1,3 +1,30 @@
+/**
+ * Adds Keyboard navigation to HTML elements through data attributes.
+ * 
+ * User behavior would be tabbing to the container UL and then keying down and 
+ * up through the list element. Once in the list, tabbing again would either 
+ * navigate to a tabable element in that list item or the next tabable element 
+ * in the DOM.
+ * 
+ * Example Usage:
+ * ```
+ * // JS: Once DOM ready
+ * knw.keynav.init()    // Or if using this file directly: Keynav.init()
+ * 
+ * // JS: Add Behavior?
+ * // Each list item has a listener to trigger a click on keying Enter or Space.
+ * // This way any click listeners on a list element would fire.
+ * 
+ * // HTML: add keynav to a list using data-knw-keynav-list
+ * <h2 id="keynavUpDown-label">List of Items - nav with up or down keys</h2>
+ * <ul data-knw-keynav-list tabindex="0" id="keynavUpDown-list" aria-labelledby="keynavUpDown-label">
+ *  <li tabindex="-1">List Item A</li>
+ *  <li tabindex="-1">List Item B</li>
+ *  <li tabindex="-1">List Item C</li>
+ * </ul>
+ * ```
+ */
+
 const Keynav = {};
 
 Keynav.init = function(el) {
